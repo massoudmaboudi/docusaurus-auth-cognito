c629697b1ca367fb12898ecfaedbcc796de56f05
Massoud Maboudi <massoud.maboudi@gmail.com>
2023-06-14 09:52:58 +0800

    hotfix: added the direct route for login path

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,21 +1,14 @@
 // components/Login.js
-import React, { useState } from "react";
+import React from "react";
 
 import { Authenticator, View } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
-import { Redirect, useHistory, useLocation } from '@docusaurus/router';
+import { Redirect } from '@docusaurus/router';
 
 import './styles.css';
-import { BASE, LOGIN_PATH } from "../../utils/constants";
 
-export function Login() {
-
-    const location = useLocation();
-    const history = useHistory();
-
-    let from = location.pathname;
-    let to = history.location.pathname === LOGIN_PATH ? BASE : history.location.pathname;
+export function Login({ from, to }) {
 
     return (
         <View className="auth-wrapper">
